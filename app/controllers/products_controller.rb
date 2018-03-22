b0c3e0857993465ff40a030e80760e0b43b5f943
Unai Martinez <unaimartinezrodriguez@gmail.com>
2018-03-22 15:53:24 +0100

    Add new tag to the product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,18 @@ class ProductsController < ApplicationController
     redirect_to product_path(params[:product_id])
   end
 
+  def add_tag
+    tag_array = [params['tag'][:new_tag].sub('%20', ' ')]
+    params['tag'][:old_tags].split(' ').each do |tag|
+      tag_array << tag.sub('%20', ' ')
+    end
+    tags = {
+      'tags' => tag_array
+    }
+    BeyondPublicApi::Tags.update_tags(params[:product_id], tags)
+    redirect_to product_path(params[:product_id])
+  end
+
   def about
     @word = ENV['TEST']
     @number = params[:number].to_i
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get '/about/:number', to: 'products#show_number', as: 'about_number'
   get '/update_tags/:product_id/:tags', to: 'products#update_tags', as: 'update_tags'
+  post '/add_tag/:product_id', to: 'products#add_tag', as: 'add_tag'
   resources :products
 end
